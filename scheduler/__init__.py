8572d5dd0ff0ada1072fb174dd9bffb43b2a4686
Suhana84158 <sanathoikangujam1@gmail.com>
2026-07-11 18:43:41 +0530

    Update __init__.py

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -1,2 +1,27 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from .scheduler import start_scheduler
+from config import TIMEZONE, EXPIRY_CHECK_INTERVAL_MINUTES
+from scheduler_jobs.expiry import check_expired_subscriptions
+
+
+scheduler = AsyncIOScheduler(timezone=TIMEZONE)
+
+
+def start_scheduler():
+    if scheduler.running:
+        return
+
+    scheduler.add_job(
+        check_expired_subscriptions,
+        trigger="interval",
+        minutes=EXPIRY_CHECK_INTERVAL_MINUTES,
+        id="subscription_expiry_check",
+        replace_existing=True,
+    )
+
+    scheduler.start()
+
+
+def shutdown_scheduler():
+    if scheduler.running:
+        scheduler.shutdown(wait=False)
